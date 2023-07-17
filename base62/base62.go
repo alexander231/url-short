2c54f0c30ba0cca6859623284200b41db2188fbd
alexander231 <ggal2426@gmail.com>
2023-07-17 23:33:43 +0300

    format encode package

--- base62/base62.go
+++ base62/base62.go
@@ -1,6 +1,8 @@
 package base62
 
-import "strings"
+import (
+	"strings"
+)
 
 const (
 	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -10,7 +12,6 @@ func Encode(n uint64) string {
 	length := uint64(len(alphabet))
 	var encodedBuilder strings.Builder
 	encodedBuilder.Grow(10)
-
 	for n > 0 {
 		remainder := n % length
 		encodedBuilder.WriteByte(alphabet[remainder])
